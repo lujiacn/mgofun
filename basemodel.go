9c8c0e5531494d5af7f477bd0d2d66016eb6d1cc
Jia Lu <lujiacn@gmail.com>
2017-07-02 19:37:07 +0800

    break update, changed BaseMod to BaseModel

--- basemodel.go
+++ basemodel.go
@@ -12,22 +12,8 @@ const (
 	DELETE = iota
 )
 
-// seperate by _
 // BaseModel to be emmbered to other struct as audit trail perpurse
 type BaseModel struct {
-	Id        bson.ObjectId `bson:"_id,omitempty"`
-	CreatedAt time.Time     `bson:"created_at,omitempty"`
-	CreatedBy string        `bson:"created_by,omitempty"`
-	UpdatedAt time.Time     `bson:"updated_at,omitempty"`
-	UpdatedBy string        `bson:"updated_by,omitempty"`
-	IsRemoved bool          `bson:"is_removed,omitempty"`
-	RemovedAt time.Time     `bson:"removed_at,omitempty"`
-	RemovedBy string        `bson:"removed_by,omitempty"`
-}
-
-// Camel case
-// BaseModel to be emmbered to other struct as audit trail perpurse
-type BaseMod struct {
 	Id        bson.ObjectId `bson:"_id,omitempty"`
 	CreatedAt time.Time     `bson:"CreatedAt,omitempty"`
 	CreatedBy string        `bson:"CreatedBy,omitempty"`
@@ -40,7 +26,7 @@ type BaseMod struct {
 
 //ChangeLog
 type ChangeLog struct {
-	BaseMod      `bson:",inline"`
+	BaseModel    `bson:",inline"`
 	ModelObjId   bson.ObjectId `bson:"ModelObjId,omitempty"`
 	ModelName    string        `bson:"ModelName,omitempty"`
 	ModelValue   interface{}   `bson:"ModelValue,omitempty"`
